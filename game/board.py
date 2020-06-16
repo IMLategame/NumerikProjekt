c609e81b4de65cfad7695eb2e7eef997abac93fe
Tobias Nauen <tobias.nauen@gmail.com>
2020-06-16 22:26:03 +0200

    start for player interface

--- game/board.py
+++ game/board.py
@@ -1,10 +1,11 @@
 
-class board:
+class Board:
     def __init__(self):
         # possible states:
         self.empty = (0, 0)
         self.p0 = (1, 0)
         self.p1 = (0, 1)
+        self.player_map = {0: self.p0, 1: self.p1}
 
         # board is a matrix 3x3x3 of (0,0). Non in unnecessary matrix entries.
         # Access by [ring, x, y] with inner ring = 0, middle ring = 1, outer ring = 2.
@@ -23,13 +24,12 @@ class board:
 
     def get_player_pos(self, player):
         assert player in [0,1]
-        player_map = {0 : self.p0, 1 : self.p1}
         pos = []
         for x in range(3):
             for y in range(3):
                 if x == 2 and y == 2:
                     continue
                 for r in range(3):
-                    if self.board_state[r, x, y] == player_map[player]:
+                    if self.board_state[r, x, y] == self.player_map[player]:
                         pos.append((r, x, y))
         return pos
---
+++ game/players/playerI.py
@@ -0,0 +1,14 @@
+from game.board import Board
+
+class PlayerI:
+    def __init__(self, playerID=0):
+        assert playerID in [0,1]
+        self.playerID = playerID
+
+    def setPlayerID(self, ID):
+        assert ID in [0,1]
+        self.playerID = ID
+
+    def getMove(self, phase, board: Board):
+        assert phase in ["set", "move", "end"]
+        raise NotImplementedError()
