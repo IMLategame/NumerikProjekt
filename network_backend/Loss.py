bc9dcffb11d31cd0c54a0dbf6bf60d764b3af8fc
Tobias Nauen <tobias.nauen@gmail.com>
2020-07-08 17:08:00 +0200

    some loss functions

--- network_backend/Loss.py
+++ network_backend/Loss.py
@@ -1,5 +1,30 @@
-class LossI:
+import numpy as np
 
-    #return is loss, delta
+
+# interface for loss functions
+class LossI:
     def loss(self, out, labels):
         raise NotImplementedError()
+
+    def d(self, out, labels):
+        raise NotImplementedError()
+
+    # return is loss, delta
+    def __call__(self, out, labels):
+        return self.loss(out, labels), self.d(out, labels)
+
+
+class L2Loss(LossI):
+    def loss(self, out, labels):
+        return 1/2*(labels-out).T @ (labels-out)
+
+    def d(self, out, labels):
+        return (out - labels).T @ np.ones(labels.shape)
+
+
+class BCELoss(LossI):
+    def loss(self, out, labels):
+        return -(labels * np.log2(out) + (1-labels) * np.log2(1-out))
+
+    def d(self, out, labels):
+        return (1-labels)/(1-out) - labels/out
