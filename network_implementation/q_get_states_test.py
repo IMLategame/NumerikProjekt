d4a00d87cd79fcca31333b75a3ed34b8db282531
Tobias Nauen <tobias.nauen@gmail.com>
2020-07-24 16:13:42 +0200

    added evaluation steps

--- network_implementation/q_get_states_test.py
+++ network_implementation/q_get_states_test.py
@@ -8,7 +8,7 @@ sys.path.insert(1, str(path))
 from network_backend.Loss import L2Loss
 from network_backend.NonLinear import Tanh
 from network_backend.Optimizers import Adam
-from game.players import QNetPlayer
+from game.players import QNetPlayer, RandomPlayer
 from network_backend.reinforcement_learning.utils import ReplayMem
 from network_backend.reinforcement_learning.encodings import QEncoding
 from game.gamestate import Game
@@ -22,37 +22,52 @@ net = FullyConnectedNet([103, 100, 50, 200, 50, 10, 1], nonLin=Tanh())
 
 player0 = QNetPlayer(net, 0)
 player1 = QNetPlayer(net, 1)
-memory = ReplayMem(1000, 1, net, 0.9, encode=QEncoding(), goal_value_function=QGoal())
-reward = SimpleReward(take_factor=1.0, penalty=0.1, win_reward=10.0)
+memory = ReplayMem(1000, 1, net, gamma=0.99, encode=QEncoding(), goal_value_function=QGoal())
+reward = SimpleReward(take_factor=10.0, penalty=0.01, win_reward=100.0)
 game = Game(run=False, p0=player0, p1=player1, mem=memory, reward=reward)
 
 opt = Adam(net)
 criterion = L2Loss()
 batch_size = 30
-epochs = 100
-epoch = 200
-#plt.axis([0, epochs+epoch, 0, 10])
+epochs = 1000
+plt.axis([0, epochs, 0, 10])
 start = time.time()
 losses = []
-plt.axis([0, epochs, 0, 10])
-while True:
+evaluation_epochs = 10
+evaluation_games = 10
+assert epochs % evaluation_epochs == 0
+plt.axis([0, int(epochs/evaluation_epochs), 0, 1])
+for epoch in range(epochs):
     if epoch % epochs == 0:
         with open("networks/q_learning/morris_ep_{}.net".format(epoch), "w+") as f:
             net.toFile(f)
+    print("Epoch {}".format(epoch))
     n = game.learn(0.1)
-    print("Epoch {}\n\tPlayed for {} steps".format(epoch, n))
+    print("\tPlayed for {} steps".format(n))
     batcher = SimpleBatcher(batch_size, memory.get_data())
     for x, y in batcher:
         out = net(x)
         loss, delta = criterion(out, y)
         net.backprop(delta)
         opt.take_step()
-    batch_avrg_loss = sum(loss)/batch_size
-    losses.append(batch_avrg_loss)
-    print(batch_avrg_loss)
-    #plt.scatter(epoch, batch_avrg_loss)
-    #plt.pause(0.05)
-    epoch += 1
+    # Evaluation and plotting:
+    if epoch % evaluation_epochs == 0:
+        print("Evaluating")
+        wins = 0
+        g = Game(p0=player0, p1=RandomPlayer(1 - player0.playerID), run=False)
+        for _ in range(evaluation_games):
+            g.play()
+            if g.winner.playerID == player0.playerID:
+                wins += 1
+        g = Game(p0=RandomPlayer(1 - player1.playerID), p1=player1, run=False)
+        for _ in range(evaluation_games):
+            g.play()
+            if g.winner.playerID == player1.playerID:
+                wins += 1
+        win_percent = wins/(2 * evaluation_games)
+        print("\tWon {}% of games".format(win_percent * 100))
+        plt.scatter(int(epoch/evaluation_epochs), win_percent)
+        plt.pause(0.05)
 end = time.time()
 time_diff = end-start
 print("Trained for \n\t {} s \n \t {} epochs\n \t {} s/epoch".format(time_diff, epoch+1, time_diff/(epoch+1)))
