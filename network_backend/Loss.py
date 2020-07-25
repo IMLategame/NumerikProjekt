39a45078e28ae3d8c3f980b883fc018160589373
Tobias Nauen <tobias.nauen@gmail.com>
2020-07-25 20:19:26 +0200

    CrossEntropyLoss and SplitLoss implemented

--- network_backend/Loss.py
+++ network_backend/Loss.py
@@ -29,7 +29,41 @@ class BCELoss(LossI):
         self.eps = eps
 
     def loss(self, out, labels):
-        return -(labels * np.log2(out+self.eps) + (1-labels) * np.log2(1-out+self.eps))
+        return -(labels * np.log(out+self.eps) + (1-labels) * np.log(1-out+self.eps))
 
     def d(self, out, labels):
         return (1 - labels) / (1 - out + self.eps) - labels / (out + self.eps)
+
+
+class CrossEntropyLoss(LossI):
+    def __init__(self, eps=1e-10):
+        self.eps = eps
+
+    def loss(self, out, labels):
+        return -np.sum(labels * np.log(out), axis=0)
+
+    def d(self, out, labels):
+        return -labels/(out + self.eps)
+
+
+class SplitLosses(LossI):
+    def __init__(self, sizes, losses):
+        assert len(sizes) > 0
+        assert len(sizes) == len(losses)
+        for loss in losses:
+            assert isinstance(loss, LossI)
+        self.sizes = sizes
+        self.split_sizes = [sum(self.sizes[:i + 1]) for i in range(len(self.sizes))]
+        self.losses = losses
+
+    def loss(self, out, labels):
+        split_out = np.split(out, self.split_sizes, axis=0)
+        split_labels = np.split(labels, self.split_sizes, axis=0)
+        split_out = [loss.loss(out, labels) for loss, out, labels in zip(self.losses, split_out, split_labels)]
+        return np.concatenate(split_out, axis=0)
+
+    def d(self, out, labels):
+        split_out = np.split(out, self.split_sizes, axis=0)
+        split_labels = np.split(labels, self.split_sizes, axis=0)
+        split_out = [loss.d(out, labels) for loss, out, labels in zip(self.losses, split_out, split_labels)]
+        return np.concatenate(split_out, axis=0)
